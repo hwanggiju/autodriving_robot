9d6890a3a7c7d4a2e1413e3bf1a9a8c287425c67
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-01-19 23:35:14 +0900

    real real last modify

--- facerec_from_webcam_faster.py
+++ facerec_from_webcam_faster.py
@@ -58,18 +58,18 @@ if cap.isOpened():
 
         process_this_frame = not process_this_frame
 
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        '''    for (top, right, bottom, left), name in zip(face_locations, face_names):
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-        cv2.imshow('Video', frame)
+            cv2.imshow('Video', frame)'''
 
         if cv2.waitKey(1) & 0xFF == 27:
             break
