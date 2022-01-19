f96a122d40c50702f03569451add864d22fada8c
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-01-19 16:26:04 +0900

    modify

--- facerec_from_webcam.py
+++ facerec_from_webcam.py
@@ -1,26 +1,20 @@
 import face_recognition
 import cv2
 import numpy as np
-import gc
 cap = cv2.VideoCapture(0)
 
-obama_image = face_recognition.load_image_file("obama.jpg")
+obama_image = face_recognition.load_image_file("obama_small.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
-biden_image = face_recognition.load_image_file("biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
-
-giju_image = face_recognition.load_image_file("giju_image.jpg")
+giju_image = face_recognition.load_image_file("giju_small_image.jpg")
 giju_face_encoding = face_recognition.face_encodings(giju_image)[0]
 
 known_face_encodings = [
     obama_face_encoding,
-    biden_face_encoding,
     giju_face_encoding
 ]
 known_face_names = [
     "Barack Obama",
-    "Biden",
     "giju"
 ]
 
@@ -46,16 +40,14 @@ if cap.isOpened():
                 name = known_face_names[best_match_index]
             print(name)
 
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+#            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+#            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+#            font = cv2.FONT_HERSHEY_DUPLEX
+#            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
         if cv2.waitKey(delay) & 0xFF == 27:
             break
-        cv2.imshow("Video", frame)
-
-gc.collect(generation=2)
+#        cv2.imshow("Video", frame)
 cap.release()
 cv2.destroyAllWindows()
