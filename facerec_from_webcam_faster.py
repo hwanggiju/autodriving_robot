3bb48b46558c866ccf43eaa4729573f1a2798531
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-01-20 13:09:46 +0900

    1/20 1차 수정

--- facerec_from_webcam_faster.py
+++ facerec_from_webcam_faster.py
@@ -4,7 +4,7 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-obama_image = face_recognition.load_image_file("obama.jpg")
+obama_image = face_recognition.load_image_file("obama_small.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 biden_image = face_recognition.load_image_file("biden.jpg")
