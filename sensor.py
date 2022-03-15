32a699bddc97b03ae612a5a03a3e6c1b576f2be7
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-03-15 17:25:03 +0900

    3/15
    sensor.py 2차 수정

--- sensor.py
+++ sensor.py
@@ -7,7 +7,7 @@ ser = serial.Serial(port, brate, timeout=None)
 
 while True :
     try :
-        data = port.readline()
+        data = ser.readline()
         
         print('distance : ')
         print(data)
