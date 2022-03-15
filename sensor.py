8d3b969dc2248e1861ba97057d6b7b0e4bf2c626
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-03-15 18:53:34 +0900

    3/15
    sensor.py 수정

--- sensor.py
+++ sensor.py
@@ -8,9 +8,10 @@ ser = serial.Serial(port, brate, timeout=None)
 while True :
     try :
         data = ser.readline()
+        data = data.encode('utf-8')
         
         print('distance : ')
-        print(data[2:8])
+        print(data)
         
     except KeyboardInterrupt:
         break
