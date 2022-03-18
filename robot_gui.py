35016d9af5bac789dbab9e1e5a43a1aa3eae65ae
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-03-18 16:30:57 +0900

    modify
    3/18

--- robot_gui.py
+++ robot_gui.py
@@ -178,8 +178,8 @@ class Motor_tab(QWidget):
         while cnt < 10 :
             data = self.ser.readline()
             if data :
-#            self.text_edit.append(data.decode()[:len(data)-1])
-                self.text_edit.setText(data.decode()[:len(data)-1])
+                self.text_edit.append(data.decode()[:len(data)-1])
+#                self.text_edit.setText(data.decode()[:len(data)-1])
                 cnt += 1
     def clearText(self) :
         self.text_edit.clear()
