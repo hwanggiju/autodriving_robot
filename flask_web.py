1ae51a7c9d995f60f34265925902ed0a1af42db1
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-03-21 22:30:07 +0900

    웹 제작
    학교 공용 와이파이 환경으로 바뀌어 포트포워딩이 힘들어 웹제작으로 변경

---
+++ flask_web.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello() :
+    return 'hello world'
+
+@app.route("/hello/")
+def hello_flask() :
+    return "Hello Flask!"
+
+if __name__=="__main__":
+    app.run()
