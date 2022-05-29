f4e948895b95067ca4865d9bde732083110f7fdd
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-05-29 18:01:15 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import serial
 
-ch = input()
 
 port = '/dev/ttyACM0'
 brate = 9600
 ser = serial.Serial(port, brate, timeout=None)
 while True :
+    ch = input()
     if ch == 'g' :
         ser.write(ch.encode())
         
