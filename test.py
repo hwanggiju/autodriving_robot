859fcdb450ca3b1d73ae68c59c71ae23c28c141f
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-01 17:42:23 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,9 @@ brate = 9600
 ser = serial.Serial(port, brate, timeout=None)
 while True :
     ch = input()
+    
     if ch == 'g' :
+        print(type(ch))
         ser.write(ch.encode())
         
     if ch == 's' :
