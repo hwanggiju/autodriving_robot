ca26044f35b624fe0458cafe89e2121576e28d31
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-07-04 14:29:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,183 +14,174 @@ from random import random
 import json
 import os
 
-global user, switch, name
-user = 0 
-switch = 1 
-name = 'Unknown'
+try :
+    global user, switch, name
+    user = 0 
+    switch = 1 
+    name = 'Unknown'
 
-# 이미지 학습 전처리
-# C:/opencv/development/face/
-obama_image = face_recognition.load_image_file("image_dir/obama.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+    # 이미지 학습 전처리
+    # C:/opencv/development/face/
+    obama_image = face_recognition.load_image_file("image_dir/obama.jpg")
+    obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
-biden_image = face_recognition.load_image_file("image_dir/biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+    biden_image = face_recognition.load_image_file("image_dir/biden.jpg")
+    biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
-giju_image = face_recognition.load_image_file("image_dir/giju_image.jpg")
-giju_face_encoding = face_recognition.face_encodings(giju_image)[0]
+    giju_image = face_recognition.load_image_file("image_dir/giju_image.jpg")
+    giju_face_encoding = face_recognition.face_encodings(giju_image)[0]
 
-known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding,
-    giju_face_encoding
-]
-known_face_names = [
-    "Barack Obama",
-    "Joe Biden",
-    "Gi ju"
-]
-# 어플리케이션 선언
-app = Flask(__name__, static_url_path='/static')
+    known_face_encodings = [
+        obama_face_encoding,
+        biden_face_encoding,
+        giju_face_encoding
+    ]
+    known_face_names = [
+        "Barack Obama",
+        "Joe Biden",
+        "Gi ju"
+    ]
+    # 어플리케이션 선언
+    app = Flask(__name__, static_url_path='/static')
 
-cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0)
 
-def user_detect(frame) :
-    global name
-    rgb_frame = frame[:, :, ::-1]
-    
-    face_locations = face_recognition.face_locations(rgb_frame) # 찾은 얼굴 값
-    
-    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations) # 얼굴 인코딩
-    
-    for face_encoding in face_encodings:
-    
-        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+    # 사용자 얼굴 감지 구현 함수
+    def user_detect(frame) : 
+        global name
+        rgb_frame = frame[:, :, ::-1]
         
-        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-        best_match_index = np.argmin(face_distances)
-        if matches[best_match_index]:
-            name = known_face_names[best_match_index]
-            return name
+        face_locations = face_recognition.face_locations(rgb_frame) # 찾은 얼굴 값
         
-    return name
-    
-def gen_frame(cap):
-    global user
-    
-    while True:
-        success, frame = cap.read()
-        if success:
-            if (user) :
-                user = 0
-                name = user_detect(frame)
-            try :
-                ret, jpeg = cv2.imencode('.jpg', cv2.flip(frame, 1)) # 프레임 -> 메모리 버퍼로 인코딩
-                frame = jpeg.tobytes()
-                yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n') # HTTP 응답으로 전송하는데 필요한 형식으로 전환
-            except Exception as e:
-                pass
-        else :
-            pass
-
-def serial_start(ch) :
-    port = '/dev/ttyACM0'
-    brate = 9600
-    ser = serial.Serial(port, brate, timeout=None)
-    
-    while True :
-        cmd = ch
-        if cmd == 'stop' :
-            ser.write(cmd.encode())
-        if cmd == 'front' :
-            ser.write(cmd.encode())  
-        if cmd == 'left' :
-            ser.write(cmd.encode()) 
-        if cmd == 'right' :
-            ser.write(cmd.encode()) 
-        if cmd == 'back' :
-            ser.write(cmd.encode()) 
-        if cmd == 'reset' :
-            ser.write(cmd.encode()) 
-        if cmd == 'Pos' :
-            ser.write(cmd.encode()) 
-
-
-@app.route('/')
-def index():
-    return render_template('main.html')
-
-@app.route('/Sensor')
-def bridge_sensor():
-    return render_template('index.html')
-
-@app.route("/Map")
-def bridge_map():
-    # 0 : no data, 1 : 라이다 센싱, 2 : 벽, 3 : 목적지, 4 : 빈공간, 5 : 현재 로봇 위치, 6 : 경로 
-    with open("map.txt", mode = "rt", encoding = 'utf-8') as f :
-        line = f.readlines()
-        tmp_lst = [[] for i in range(1000)]
-        item_lst = list(line) # 문자열 리스트에 저장
-       
-    return render_template('map.html', string_lst = item_lst)
-
-@app.route("/Control")
-def control() :
-    return render_template('test.html')
-
-@app.route('/live-data')
-def live_data():
-    '''
-    port = '/dev/ttyACM0'
-    brate = 9600
-    ser = serial.Serial(port, brate, timeout=None)
-    senser_data = ser.readline()
-    senser_data = float(data.decode()[:len(data)-3])
-    '''
-    data = [time() * 1000, random() * 100]
-    response = make_response(json.dumps(data))
-    response.content_type = 'application/json'
-    return response
-
-@app.route('/stream')
-def stream():
-    global cap
-    return Response(gen_frame(cap), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/requests', methods=['POST', 'GET'])
-def tasks() :
-    global switch, name, cap
-    
-    if request.method == 'POST' :
-        if request.form.get('clicked') == 'User':
-            global user
-            user = 1
+        face_encodings = face_recognition.face_encodings(rgb_frame, face_locations) # 얼굴 인코딩
+        
+        for face_encoding in face_encodings:
+        
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             
-        elif request.form.get('camera_stop') == 'Stop/Start':
-            if(switch==1):
-                switch = 0
-                cap.release()
-                cv2.destroyAllWindows()
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+                return name
+            
+        return name
+        
+    # 카메라 frame 읽어오고 웹에서 표현하는 형식으로 인코딩해주는 함수
+    def gen_frame(cap):
+        global user
+        
+        while True:
+            success, frame = cap.read()
+            if success:
+                if (user) :
+                    user = 0
+                    name = user_detect(frame)
+                try :
+                    ret, jpeg = cv2.imencode('.jpg', cv2.flip(frame, 1)) # 프레임 -> 메모리 버퍼로 인코딩
+                    frame = jpeg.tobytes()
+                    yield (b'--frame\r\n'
+                        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n') # HTTP 응답으로 전송하는데 필요한 형식으로 전환
+                except Exception as e:
+                    pass
             else :
-                cap = cv2.VideoCapture(0)
-                switch = 1
-                
-    elif request.method == 'GET' :
+                pass
+
+    # 웹 기본 화면 - 카메라 실시간 화면 표시
+    @app.route('/')
+    def index():
         return render_template('main.html')
-    
-    return render_template('main.html', value = name)
-    
-@app.route('/Control/requests1', methods=['POST'])
-def gostop() :
-    port = '/dev/ttyACM0'
-    brate = 9600
-    ser = serial.Serial(port, brate, timeout=None)
-    
-    if request.method == 'POST' :
-        ch = request.form.get('name')
 
-        if ch == 'g' :
-            ser.write(ch.encode())
-            senser_data = ser.readline()
-            print(senser_data.decode()[:len(senser_data)-2])
-            
-        if ch == 's' :
-            ser.write(ch.encode())
-            senser_data = ser.readline()
-            print(senser_data.decode()[:len(senser_data)-2])
+    # 데이터 그래프 화면 - 모터값을 표시
+    @app.route('/Sensor')
+    def bridge_sensor():
+        return render_template('index.html')
+
+    # 지도 그려주는 화면 - 현재 미완성 작성중
+    @app.route("/Map")
+    def bridge_map():
+        # 0 : no data, 1 : 라이다 센싱, 2 : 벽, 3 : 목적지, 4 : 빈공간, 5 : 현재 로봇 위치, 6 : 경로 
+        with open("map.txt", mode = "rt", encoding = 'utf-8') as f :
+            line = f.readlines()
+            tmp_lst = [[] for i in range(1000)]
+            item_lst = list(line) # 문자열 리스트에 저장
+        
+        return render_template('map.html', string_lst = item_lst)
+
+    # 명령 입력 화면 - 현재 미완성 작성중
+    @app.route("/Control")
+    def control() :
+        return render_template('test.html')
+
+    # 데이터 그래프 실시간으로 그려주는 동적 함수 - 자바 코드와 연동됨
+    @app.route('/live-data')
+    def live_data():
+        '''
+        port = '/dev/ttyACM0'
+        brate = 9600
+        ser = serial.Serial(port, brate, timeout=None)
+        senser_data = ser.readline()
+        senser_data = float(data.decode()[:len(data)-3])
+        '''
+        data = [time() * 1000, random() * 100]
+        response = make_response(json.dumps(data))
+        response.content_type = 'application/json'
+        return response
+
+    # 카메라 실시간 스트리밍 화면 보여주는 함수
+    @app.route('/stream')
+    def stream():
+        global cap
+        return Response(gen_frame(cap), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+    # 기본 카메라 화면에서 버튼 입력으로 카메라 동작 ON/OFF 가능
+    # 사용자 인식 버튼 입력 시 사용자 얼굴 매칭
+    @app.route('/requests', methods=['POST', 'GET'])
+    def tasks() :
+        global switch, name, cap
+        if request.method == 'POST' :
+            if request.form.get('clicked') == 'User':
+                global user
+                user = 1
+                
+            elif request.form.get('camera_stop') == 'Stop/Start':
+                if(switch==1):
+                    switch = 0
+                    cap.release()
+                else :
+                    cap = cv2.VideoCapture(0)
+                    switch = 1
                     
-    return render_template('test.html')
+        elif request.method == 'GET' :
+            return render_template('main.html')
+        
+        return render_template('main.html', value = name)
+        
+    # 모터 동작 명령 수행
+    # stop, front, left, right, back, reset, Pos 명령어
+    @app.route('/Control/requests1', methods=['POST'])
+    def gostop() :
+        port = '/dev/ttyACM0'
+        brate = 9600
+        ser = serial.Serial(port, brate, timeout=None)
+        
+        if request.method == 'POST' :
+            ch = request.form.get('name')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True, debug=True)
+            if ch == 'g' :
+                ser.write(ch.encode())
+                senser_data = ser.readline()
+                print(senser_data.decode()[:len(senser_data)-2])
+                
+            if ch == 's' :
+                ser.write(ch.encode())
+                senser_data = ser.readline()
+                print(senser_data.decode()[:len(senser_data)-2])
+                        
+        return render_template('test.html')
+
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', threaded=True, debug=True)
+        
+except KeyboardInterrupt :
+    pass
