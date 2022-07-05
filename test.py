3cebe06901aade2f4a531ba9759dd5fe9f08080b
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-07-05 12:28:47 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ brate = 9600
 ser = serial.Serial(port, brate, timeout=None)
 while True :
     ch = input()
-    
+    ch = int(ch)
     if ch == 1 :
         print(type(ch))
         ser.write(ch.encode())
