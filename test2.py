9aa42f10d2f7bc1469623ea0edefa673b5a57bbe
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-08-03 18:38:42 +0900

    .

--- test2.py
+++ test2.py
@@ -1,14 +1,22 @@
-import cv2
+import sys
 
-cap = cv2.VideoCapture(0)
+def fib(n) :
+    if (n == 1 or n == 2) :
+        return 1
+    
+    else :
+        return (fib(n-1) + fib(n-2)) 
+    
+def fibonacci(n) :
+    dp = [0] * (n+1) 
+    dp[1] = 1
+    dp[2] = 1
+    cnt = 0
+    for i in range(3, n + 1) :
+        cnt += 1
+        dp[i] = dp[i-1] + dp[i-2]
+    return cnt
 
-while True :
-    _, frame = cap.read()
-    
-    cv2.imshow('test', frame)
-    
-    if cv2.waitKey() & 0xFF == 27 :
-        break
-    
-cap.release()
-cv2.destroyAllWindows()
+n = int(sys.stdin.readline())
+
+print(fib(n), fibonacci(n))
