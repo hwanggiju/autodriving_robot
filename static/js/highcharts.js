9de94ca1aa67de850523ea3eb13e281583c4d222
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-08-09 09:45:04 +0900

    Update highcharts.js

--- static/js/highcharts.js
+++ static/js/highcharts.js
@@ -1,20 +1,23 @@
 var chart;
-
+// 데이터 요청
 function requestData() {
     $.ajax({
+        // 웹페이지와 연결되는 url
         url: '/live-data',
+        // 데이터가 들어오면 실행되는 함수
         success: function(point) {
             var series = chart.series[0],
+                // 한 화면 그래프에 그려지는 데이터 최대 개수 20개 설정
                 shift = series.data.length > 20; 
-
+            // 다음 포인트 지정
             chart.series[0].addPoint(point, true, shift);
-
+            // delay 1초
             setTimeout(requestData, 1000);
         },
         cache: false
     });
 }
-
+// css 기반 그래프 x, y, 제목 등 기본 지정
 $(document).ready(function() {
     chart = new Highcharts.Chart({
         chart: {
